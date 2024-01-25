e761cbc56e9ce22221f34c829af68a5027560e49
AllanQD <allan.estonia@gmail.com>
2024-01-25 11:50:01 +0200

    HTMLCSS kodutoo

---
+++ esimene/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <button>Vajuta, vali järgmine</button>
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ veebipood/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <img className="pilt" src='https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1562246977/autoexpress/2018/10/bmw_z1_1_0.jpg' alt="Nobe Elektriauto"></img> 
+     <button className="nupp">Vajuta</button>
+<p>tekst</p>
+<div className="tekst">Tekst2</div>
+    </div>
+  );
+}
+
+export default App;
